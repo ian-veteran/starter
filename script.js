3daf14019b7bf3e716d8bcebf44364f9f355b0ac
ian-veteran <65036226+ian-veteran@users.noreply.github.com>
2024-01-25 21:50:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,41 @@
 const secretNumber=Math.trunc(Math.random()*20)+1;
 let score = 0;
 let highScore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+  };
+
 document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function(){
 
     const guess = Number(document.querySelector('.guess').value);
 
     if (!guess){
-        document.querySelector('.message').textContent = 'No Number'
+        displayMessage('No Number');
     } else if (guess === secretNumber){
-        document.querySelector('.message').textContent = 'Correct Number mkuu';
+        displayMessage('Correct Number mkuu');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
     } else if (guess > secretNumber){
-        document.querySelector('.message').textContent = 'Too High';
+        displayMessage('Too High');
     } else if (guess < secretNumber){
-        document.querySelector('.message').textContent = 'Too low';
+        displayMessage('Too low');
     }
 
+});
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    
+    displayMessage('Start guessing...');
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    
+  
 });
